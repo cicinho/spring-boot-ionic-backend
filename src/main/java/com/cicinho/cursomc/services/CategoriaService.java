ffe16a9b75366cc87e2236bb4e4f8c909cd10813
Cicero Alves <cicerojprn@gmail.com>
2018-10-15 12:43:33 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/cicinho/cursomc/services/CategoriaService.java
+++ src/main/java/com/cicinho/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cicinho.cursomc.domain.Categoria;
+import com.cicinho.cursomc.dto.CategoriaDTO;
 import com.cicinho.cursomc.repositories.CategoriaRepository;
 import com.cicinho.cursomc.services.exceptions.DataIntegrityException;
 import com.cicinho.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
